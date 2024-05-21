2679e1236194590376876b6ccd3f1a87cc913c98
Maria Reyna <maria.reyna2244@gmail.com>
2024-05-21 09:28:56 -0400

    Initial Commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Maria Reyna on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
